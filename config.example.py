1b7db946fdb86da926df104bcad1de2e7dbaecab
Christopher Groskopf <staringmonkey@gmail.com>
2010-10-09 16:48:36 -0500

    Added missing file.

---
+++ config.example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+DOCUMENTS = {
+    'boilertest': {
+        # Google Docs key (from url)
+        'key': '0AlXMOHKxzQVRdGt5aWV0UG9XaWVIVWVCYWRqVi11dGc',
+        # Number of seconds to keep in memcache before refetching
+        'ttl': 15,
+        # Convert to JSON server-side?
+        'convert': False
+    }
+}
